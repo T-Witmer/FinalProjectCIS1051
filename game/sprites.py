1f7fe8b56159b23c8de3200c4cb4d5336a7d29ab
Troy Witmer <91856253+T-Witmer@users.noreply.github.com>
2022-04-17 21:52:01 -0400

    update

--- game/sprites.py
+++ game/sprites.py
@@ -1,4 +1,5 @@
 import pygame
+import math
 from settings import *
 vec = pygame.math.Vector2
 
@@ -10,7 +11,7 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image,(width/15,height/15))
         self.rect = self.image.get_rect()
         self.rect.center = (width/2, height/ 2)
-
+        self.orig_img = self.image
         self.pos = vec(width/2, height/ 2)
         self.vel = vec(0,0)
         self.acc = vec(0,0)
@@ -56,11 +57,14 @@ class Player(pygame.sprite.Sprite):
         self.vel += self.acc
         self.pos += self.vel + .5 * self.acc
 
-     
-        
-        
-        
+        #following mouse for turning
+        pos1, pos2 = pygame.mouse.get_pos()
 
         self.rect.center = self.pos
         
-
+        _ , angle = (pygame.mouse.get_pos()-self.pos).as_polar()
+        self.image = pygame.transform.rotozoom(self.orig_img, -angle, 1)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        #got this bit from stack overflow, I could not figure uot these vectors myself
+        
+   
